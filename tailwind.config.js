8b3213404be96c78e2cf51635c544a0e7cd81542
Hunine <hqhung201@gmail.com>
2024-05-23 10:50:04 +0700

    MND-13: Topic card does not have background color
    - Fix padding in topic page
    - Reconfig tailwind
    - Remove border of topic card

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
                 primary: '#818cf8',
             },
             boxShadow: {
-                'b-1': '0 2px 0px 0px #818cf8',
+                'inner-b-6': 'inset 0 -6px 0 #818cf8',
+                'b-1': '0 1px 0px 0px #818cf8',
+                'b-2': '0 2px 0px 0px #818cf8',
             },
         },
     },
